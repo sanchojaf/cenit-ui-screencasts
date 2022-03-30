29d3c49e1ef588d213a85229a40348d8bd641438
Miguel Sancho <sanchojaf@gmail.com>
2022-03-29 20:24:11 -0400

    Update app name

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 const YOUTUBE_PLAYLIST_ITEMS_API = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
 export async function getServerSideProps() {
-  const res = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&maxResults=50&playlistId=PLFsfg2xP7cbLuAglQob6zjS4nVbyAfSVV&key=${process.env.YOUTUBE_API_KEY}`)
+  const res = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&maxResults=50&playlistId=PLY25EbO7w_Nbr5rXq5xuqaJxxxm8pmf4u&key=${process.env.YOUTUBE_API_KEY}`)
   const data = await res.json();
   return {
     props: {
@@ -17,14 +17,14 @@ export default function Home({ data }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Cenit IO</title>
+        <link rel="icon" href="/favicon.svg" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          My Talks
-        </h1>
+        <h3 className={styles.title}>
+          Getting Started
+        </h3>
 
         <ul className={styles.grid}>
           {data.items.map(({ id, snippet = {} }) => {
@@ -36,7 +36,7 @@ export default function Home({ data }) {
                   <p>
                     <img width={medium.width} height={medium.height} src={medium.url} alt="" />
                   </p>
-                  <h3>{ title }</h3>
+                  <h4>{title}</h4>
                 </a>
               </li>
             )
@@ -44,7 +44,7 @@ export default function Home({ data }) {
         </ul>
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -53,7 +53,7 @@ export default function Home({ data }) {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
